0c7aa6d50276ad507c57b6755028d590f025def5
jgrodzicki <kubagrodzicki@o2.pl>
2020-04-28 21:59:59 +0200

    connected server

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ import PodSixNet.Channel
 import PodSixNet.Server
 from time import sleep
 
+from Game import Game
+
 
 class ClientChannel(PodSixNet.Channel.Channel):
     def Network(self, data):
@@ -13,14 +15,33 @@ class PokerServer(PodSixNet.Server.Server):
 
     def __init__(self, *args, **kwargs):
         PodSixNet.Server.Server.__init__(self, *args, **kwargs)
+        self.queue = Game()
+        self.game = None
 
     def Connected(self, channel, addr):
         print('new connection:', channel)
+        self.queue.joined_player(channel)
+        print(self.queue.no_players)
 
+        if self.queue.cur_players == 2:
+            self.game = self.queue
+            self.queue = None
+
+            self.game.deal_cards()
+            for i, ch in enumerate(self.game.player_channels):
+                ch.Send({'action': 'startgame', 'init_money': 1000, 'big_blind': 50, 'players_nick': ['p1', 'p2', 'p3', 'p4', 'p5']})
+                ch.Send({'action': 'getcards', 'cards': self.game.cards[i]})
+
+
+    def fold(self, data):
+        self.game.fold(data)
 
 
 print("STARTING SERVER ON LOCALHOST")
 pokerServer = PokerServer(localaddr=('0.0.0.0', 8000))
-while True:
-    pokerServer.Pump()
-    sleep(0.01)
+try:
+    while True:
+        pokerServer.Pump()
+        sleep(0.01)
+except KeyboardInterrupt:
+    pokerServer.close()
