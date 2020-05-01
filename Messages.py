adb4657cdb52411398987c966360f5264b61fd46
jgrodzicki <kubagrodzicki@o2.pl>
2020-05-01 18:17:39 +0200

    visuals

--- Messages.py
+++ Messages.py
@@ -3,10 +3,10 @@ import pygame
 
 class Messages:
 
-    def __init__(self, location, screen, size=(250, 60)):
+    def __init__(self, location, screen, size=(275, 60)):
         self.screen = screen
         self.size = size
-        self.font = pygame.font.SysFont('Arial.ttf', 16)
+        self.font = pygame.font.SysFont('Arial', 12)
         self.txt = []
         self.txt_surf = []
         self.surface = pygame.surface.Surface(size)
@@ -18,13 +18,13 @@ class Messages:
 
     def add_text(self, text):
         self.txt.append(text)
-        self.txt_surf.append(self.font.render(text, True, (0, 0, 0), (150, 150, 150)))
+        self.txt_surf.append(self.font.render(text, True, (0, 0, 0), None))
         self.size = (250, 15*len(self.txt))
         self.surface = pygame.surface.Surface(self.size)
         self.rect = self.surface.get_rect(center=(self.location[0] + self.size[0] // 2, self.location[1] + self.size[1] // 2))
 
     def draw(self):
-        self.surface.fill((150, 150, 150))
+        self.surface.fill((220, 220, 220))
         for i in range(len(self.txt)):
             self.surface.blit(self.txt_surf[i], (0, 15*i))
             self.screen.blit(self.surface, self.location)
--- poker.py
+++ poker.py
@@ -30,7 +30,8 @@ class Poker(ConnectionListener):
         # initialize pygame clock
         self.clock = pygame.time.Clock()
 
-        self.nick_color, self.money_color, self.text_color = (100, 100, 100), (255, 255, 0), (200, 200, 200)
+        self.nick_color, self.money_color, self.text_color = (150, 150, 150), (255, 255, 0), (50, 50, 50)
+        self.bg_color = (220, 220, 220)
 
         self.is_turn = False
         self.is_playing = True
@@ -50,7 +51,8 @@ class Poker(ConnectionListener):
         self.table_img = pygame.image.load('images/table.png')
         self.table_img_x, self.table_img_y = self.width//2 - 700//2, 20
 
-        self.font = pygame.font.Font('freesansbold.ttf', 16)
+        # self.font = pygame.font.Font('freesansbold.ttf', 16)
+        self.font = pygame.font.SysFont('Arial', 16, bold=True)
         self.text_font = pygame.font.SysFont('Arial', 12)
         self.nick = nick
 
@@ -67,9 +69,9 @@ class Poker(ConnectionListener):
         self.players_nick = {}
         self.opp_ids = []
         self.opp_money = {}
-        self.players_pos = [(self.width*0.75//5, self.height*2.7//5), (self.width*0.75//5, self.height*1.5//5),
-                            (self.width//2, 50), (self.width*4.25//5, self.height*1.5//5),
-                            (self.width*4.25//5, self.height*2.7//5)]
+        self.players_pos = [(self.width*0.75//5, self.height*3.1//5), (self.width*0.75//5, self.height*1.7//5),
+                            (self.width//2, 75), (self.width*4.25//5, self.height*1.7//5),
+                            (self.width*4.25//5, self.height*3.1//5)]
 
         # cards on the table
         self.on_table = [None] * 5
@@ -82,7 +84,8 @@ class Poker(ConnectionListener):
         self.raise_b = Button('raise to', (self.width - 90, self.height-20), self.screen)
         self.raise_t = TextField((self.width - 90, self.height - 50), self.screen)
 
-        self.mess = Messages((0, self.height-115), self.screen)
+        # self.mess = Messages((0, self.height-115), self.screen)
+        self.mess = Messages((5, 5), self.screen)
 
         self.Connect((addr, int(port)))
 
@@ -349,25 +352,26 @@ class Poker(ConnectionListener):
                     self.raise_t.update(event.key)
 
     def _draw_player(self):
+        x, y = self.width//2, self.height-110
         if not self.is_out and self.card1 is not None and self.card2 is not None:
-            self.screen.blit(self.card1, (self.width // 2 - self.card_w - 5, self.height - 100))
-            self.screen.blit(self.card2, (self.width // 2 + 5, self.height - 100))
+            self.screen.blit(self.card1, (x - self.card_w - 3, y))
+            self.screen.blit(self.card2, (x + 3, y))
 
         if self.player_id == self.id_big_blind:
-            self.screen.blit(self.font.render('BB', True, self.text_color, None), (self.width // 2 + 50, self.height - 90))
+            self.screen.blit(self.font.render('BB', True, self.text_color, self.bg_color), (x+50, y+20))
 
         if self.player_id == self.id_small_blind:
-            self.screen.blit(self.font.render('SB', True, self.text_color, None), (self.width // 2 + 50, self.height - 90))
+            self.screen.blit(self.font.render('SB', True, self.text_color, self.bg_color), (x + 50, y+20))
 
         nick_label = self.font.render(self.nick, True, self.nick_color, None)
-        nick_rect = nick_label.get_rect(center=(self.width // 2, self.height - 20))
+        nick_rect = nick_label.get_rect(center=(x, y+80))
         self.screen.blit(nick_label, nick_rect)
 
         bet_label = self.font.render(str(self.bet), True, self.money_color, None)
-        self.screen.blit(bet_label, (self.width//2 + 50, self.height - 105))
+        self.screen.blit(bet_label, (x + 50, y-5))
 
         money_label = self.font.render(str(self.money), True, self.money_color, None)
-        money_rect = money_label.get_rect(center=(self.width // 2, self.height - 120))
+        money_rect = money_label.get_rect(center=(x, y-20))
         self.screen.blit(money_label, money_rect)
 
 
@@ -380,19 +384,19 @@ class Poker(ConnectionListener):
             x, y = self.players_pos[i]
 
             if not self.is_opp_out[id]:
-                self.screen.blit(self.opp_cards_img[id][0], (x-self.card_w-5, y))
-                self.screen.blit(self.opp_cards_img[id][1], (x+5, y))
+                self.screen.blit(self.opp_cards_img[id][0], (x-self.card_w-3, y))
+                self.screen.blit(self.opp_cards_img[id][1], (x+3, y))
 
             if id == self.id_big_blind:
-                self.screen.blit(self.font.render('BB', True, self.text_color, None), (x+50, y+10))
+                self.screen.blit(self.font.render('BB', True, self.text_color, self.bg_color), (x+50, y+20))
             if id == self.id_small_blind:
-                self.screen.blit(self.font.render('SB', True, self.text_color, None), (x+50, y+10))
+                self.screen.blit(self.font.render('SB', True, self.text_color, self.bg_color), (x+50, y+20))
 
             if self.id_turn == id:
-                self.screen.blit(self.font.render('turn', True, self.text_color, None), (x+50, y+40))
+                self.screen.blit(self.font.render('turn', True, self.text_color, self.bg_color), (x+50, y+40))
 
             nick_label = self.font.render(nick, True, self.nick_color, None)
-            nick_rect = nick_label.get_rect(center=(x, y+70))
+            nick_rect = nick_label.get_rect(center=(x, y+80))
             self.screen.blit(nick_label, nick_rect)
 
             bet_label = self.font.render(str(self.opp_bet[id]), True, self.money_color, None)
@@ -419,13 +423,13 @@ class Poker(ConnectionListener):
 
         if self.pot_val is not None:
             pot_val_label = self.font.render(f'pot: {int(self.pot_val)}', True, self.money_color, None)
-            pot_val_rect = pot_val_label.get_rect(center=(self.width//2, self.height//2 - 30))
+            pot_val_rect = pot_val_label.get_rect(center=(self.width//2, self.height//2 - 10))
             self.screen.blit(pot_val_label, pot_val_rect)
 
         for i, card in enumerate(self.on_table):
             if card is None:
                 break
-            self.screen.blit(card, (self.width//2 - (self.card_w//2) - (2-i)*(self.card_w+5), self.height//2))
+            self.screen.blit(card, (self.width//2 - (self.card_w//2) - (2-i)*(self.card_w+3), self.height//2))
 
 
 if __name__=='__main__':
